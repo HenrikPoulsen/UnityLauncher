00d080f19c060fce182c2c6e6a36fa715bfbcd0d
Henrik Poulsen <henrik.f.poulsen@gmail.com>
2018-11-07 17:51:46 +0100

    Experimenting with sourcing all error and warning prints from utp

---
+++ UnityLauncher.Editor/UtpLogEntryMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityLauncher.Editor {
+    public class UtpLogEntryMessage : UtpMessageBase
+    {
+        public string Message { get; set; }
+        public string Severity { get; set; }
+        public string Stacktrace { get; set; }
+        public int Line { get; set; }
+        public string File { get; set; }
+    }
+}
---
+++ UnityLauncher.Editor/UtpMessageBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityLauncher.Editor {
+    public class UtpMessageBase
+    {
+        public int Version { get; set; }
+        public string Type { get; set; }
+        public string Phase { get; set; }
+        public long Time { get; set; }
+        public int ProcessId { get; set; }
+    }
+}
